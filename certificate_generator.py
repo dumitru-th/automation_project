245001b414faae3acc975ae5271b34d2ada56a4f
Theodorescu Posea Dumitru <142658091+dumitru-th@users.noreply.github.com>
2025-07-23 12:05:57 +0300

    Update certificate_generator.py

--- certificate_generator.py
+++ certificate_generator.py
@@ -4,13 +4,13 @@ Created on Sat Sep 30 00:18:42 2023
 
 @author: Dumitru
 """
-
+print("hi")
 
 def belt_generator(kup):
     if kup==10:
         return "Alba"
     elif kup==9:
-        return "Alb cu Galben"
+        return "Alba cu Galben"
     elif kup==8:
         return "Galbena"
     elif kup==7:
@@ -44,15 +44,16 @@ with open("exam_data.txt") as file:
             try:
                 database[athlete_id]={}
                 i+=1
-                database[athlete_id]["Last Name"]=file[i].strip()
-                i+=1
-                database[athlete_id]["First Name"]=file[i].strip()
+                database[athlete_id]["Last Name"]=file[i].strip().split()[0]
+                database[athlete_id]["First Name"]=" ".join(file[i].strip().split()[1:])
+
                 i+=1
                 database[athlete_id]["Certificate ID"]=file[i].strip()
                 i+=1
                 database[athlete_id]["Old Kup Grade"]=file[i].strip()
                 i+=1
                 database[athlete_id]["New Kup Grade"]=file[i].strip().split()[1]
+                    
                 database[athlete_id]["Belt"]=belt_generator(int(file[i].strip().split()[1]))
             
                 i+=1
@@ -63,22 +64,23 @@ with open("exam_data.txt") as file:
         
            
 from docxtpl import DocxTemplate
-
+import os
 def first_name_chk(fn):
     name_to_write =""
     x=len(fn)
     if x>15:
         f_names=fn.split()
         for name in f_names:
-            if len(name)<15:
+           if  len(name_to_write+name)<=15:
                 name_to_write+=name+" "
-                x-=len(name)+1
+        
     else:
         name_to_write=fn
     return name_to_write    
                 
                 
-                 
+def get_exam_code():
+    return database[athlete_id]["Certificate ID"][:6]
 
 doc=DocxTemplate("template.docx")
 
@@ -87,29 +89,26 @@ for key in database.keys():
         context={
                 'n': database[f"{key}"]["New Kup Grade"],
                 'last_name1':database[f"{key}"]["Last Name"].upper(),
-                'first_name1':database[f"{key}"]["First Name"].upper(),
+                'first_name1':first_name_chk(database[f"{key}"]["First Name"].upper()),
                 'last_name2':database[f"{key}"]["Last Name"].upper(),
                 'first_name2':database[f"{key}"]["First Name"].upper(),
-                'belt':database[f"{key}"]["Belt"],
-                'certificate_code':database[f"{key}"]["Certificate ID"]
+                'belt':database[f"{key}"]["Belt"], 'certificate_code':database[f"{key}"]["Certificate ID"]
                 }
         doc.render(context)
-        import os
+        
 
         # Define the directory where you want to save the generated docx files
-        output_dir = "C:\\Users\\Dumitru\\Desktop\\certificate_generation\\generated_certificates"
+        output_dir = f"certificate_create\\{get_exam_code()}"
 
         # Ensure the output directory exists, if not, create it
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
         # Define the filename for the generated docx file
-        filename = f"z-{database[f'{key}']['Last Name']} {database[f'{key}']['First Name']}.docx"
-
+        filename = f"{database[f'{key}']['Last Name']} {database[f'{key}']['First Name']} kup {database[f'{key}']['New Kup Grade']}.docx"
         # Join the output directory and filename to create the full path
         output_file = os.path.join(output_dir, filename)
 
         doc.save(output_file)
        
-        
-  
+         
